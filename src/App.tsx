2b23e5a4bbce680dd757bc6c4ec3dc89a775d92c
Amirul Faqih <fakiiahmad001@gmail.com>
2025-05-30 23:47:36 +0300

    feat: Remove unused DashboardLayout import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Layout } from "@/components/Layout";
-import { DashboardLayout } from "@/components/DashboardLayout";
 import Index from "./pages/Index";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
